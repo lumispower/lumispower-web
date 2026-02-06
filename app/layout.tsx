0b7595d2ea0775e72dda39b501fc2b4d8d1b5699
Luis Celis <contacto@lumispower.cl>
2026-02-05 21:52:29 -0300

    Fix GTM initialization (dataLayer)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 ﻿import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
-
 import "../styles/globals.css";
 import Navbar from "@/components/Navbar";
 import FirstVisitModal from "@/components/FirstVisitModal";
-import { SEO_CONTENT } from "@/lib/company";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,15 +12,8 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://lumispower.cl"),
-  title: {
-    default: SEO_CONTENT.defaultTitle,
-    template: SEO_CONTENT.titleTemplate,
-  },
-  description: SEO_CONTENT.description,
-  keywords: SEO_CONTENT.keywords,
-  icons: {
-    icon: "/icon.png",
-  },
+  title: "Lumispower – Ingeniería Eléctrica Certificada",
+  description: "Instalaciones eléctricas seguras y certificadas",
 };
 
 export const viewport: Viewport = {
@@ -41,15 +31,15 @@ export default function RootLayout({
     <html lang="es" className={inter.variable}>
       <head>
         {/* Google Tag Manager */}
-        <Script
-          id="gtm"
-          strategy="beforeInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              window.dataLayer = window.dataLayer || [];
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
               f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-T82QWNZR');
             `,
@@ -57,8 +47,8 @@ export default function RootLayout({
         />
       </head>
 
-      <body className="relative bg-surface/30 text-ink">
-        {/* Google Tag Manager (noscript) */}
+      <body>
+        {/* GTM noscript */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-T82QWNZR"
